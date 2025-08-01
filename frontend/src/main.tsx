089d45e7dc70887aca59e8ad9b0c35f226e89d9c
Kirill Romanov <workrkm@mail.ru>
2025-08-01 10:04:06 +0300

    fix: off ws in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { connectRealmSocket } from './sockets/realmSockets.ts'
+//import { connectRealmSocket } from './sockets/realmSockets.ts'
 
-connectRealmSocket();
+//connectRealmSocket();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
